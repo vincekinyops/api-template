96c0b4af095d3af14559f039bee2bf678e0840fe
MarkQuinopa <mark@harken.cc>
2022-07-12 21:21:41 +0800

    added nft metadata query

--- crawler-api/routes/contract/nfts.js
+++ crawler-api/routes/contract/nfts.js
@@ -11,13 +11,21 @@ const route = async function(req, res) {
     const transactions = nftItems.map((nft) => {
       return request.get(`tokens/${address}/nft_transactions/${nft.token_id}`) 
     })
-
-    const all = await Promise.all(transactions)
-    const nfts = all.map((item) => {
-      return item?.data?.data?.items[0]
+    const txs = await Promise.all(transactions)
+    const nft_txs = txs.map((item, i) => {
+      return {...item?.data?.data?.items[0], ...nftItems[i]}
     })
 
-    res.send({ nfts });
+    const metadata = nftItems.map((nft) => {
+      return request.get(`tokens/${address}/nft_metadata/${nft.token_id}`) 
+    })
+    const metas = await Promise.all(metadata)
+    const nft_metas = metas.map((item, i) => {
+      return {...item?.data?.data?.items[0], ...nftItems[i]}
+    })
+    
+
+    res.send({ nft_txs, nft_metas });
   } catch (e) {
     res.send({ error: e.response?.data });
   }
